153995c2cbddf1f03ad1c086f6313e31f17299f3
Fred Zabulon <fredzabulon133@gmail.com>
2025-03-14 23:27:00 +0300

    currently working on the login page

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {Login} from "./pages/auth/index.js"
+function App() {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-main">
+      <Login />
+    </div>
+  )
+}
+
+export default App
