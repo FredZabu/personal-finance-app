c8be98a47bf63b252c9b517aba173fb40095060b
Fred Zabulon <fredzabulon133@gmail.com>
2025-03-18 22:53:04 +0300

    made isolations on the login component

---
+++ src/components/authImage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import auth_Image from "../assets/images/illustration-authentication.svg"
+function authImage() {
+  return (
+    <div className='grid place-items-center bg-black w-full h-[10%] rounded-b-xl lg:rounded-b-none lg:bg-transparent lg:block  lg:w-[35%] lg:h-[100%] lg:p-[3%]'>
+        <h1 className="font-logo text-white text-center lg:hidden ">finance</h1>
+        <div className='hidden lg:block relative rounded-xl  w-full h-[100%] overflow-hidden'>
+          <img className='absolute  w-full h-full object-cover' src={auth_Image} alt="" srcset="" />
+          <div className='absolute top-[2%] left-[5%] text-white text-xl'>
+            <h1 className="font-logo">finance</h1>
+          </div>
+          <div className='w-[90%] absolute bottom-[2%] left-[5%] text-white'>
+            <p className='font-h2'>Keep track of your money<br />and save for your future</p>
+            <p className='text-[12px] mt-2.5'>Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.</p>
+          </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default authImage
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,28 +1,17 @@
 import React from 'react'
-import authImage from "../../assets/images/illustration-authentication.svg"
+import { AuthImage } from '../../components/index.js'
 function login() {
   return (
     <div className='lg:flex  w-[100vw] h-[100vh]'>
-      <div className='grid place-items-center bg-black w-full h-[10%] rounded-b-xl lg:rounded-b-none lg:bg-transparent lg:block  lg:w-[35%] lg:h-[100%] lg:p-[3%]'>
-        <h1 className="font-logo text-white text-center lg:hidden ">finance</h1>
-        <div className='hidden lg:block relative rounded-xl  w-full h-[100%] overflow-hidden'>
-          <img className='absolute  w-full h-full object-cover' src={authImage} alt="" srcset="" />
-          <div className='absolute top-[2%] left-[5%] text-white text-xl'>
-            <h1 className="font-logo">finance</h1>
+      <AuthImage />
+      <div className='w-[100%] h-[100%] lg:w-[65%] grid place-items-center'>
+        <div className='bg-white w-[50%] h-[50%]'>
+          <div className='p-[4%]'>
+            <h1 className='font-h2 capitalize text-xl'>Sign up</h1>
           </div>
-          <div className='w-[90%] absolute bottom-[2%] left-[5%] text-white'>
-            <p className='font-h2'>Keep track of your money<br />and save for your future</p>
-            <p className='text-[12px] mt-2.5'>Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.</p>
-          </div>
+            
         </div>
-
       </div>
-     
-        <div className='w-[100%] h-[100%] lg:w-[65%] grid place-items-center'>
-            <div className='bg-white w-[50%] h-[50%]'>
-                <h1>login</h1>
-            </div>
-        </div>
     </div>
   )
 }
