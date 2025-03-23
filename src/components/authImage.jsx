3647bc7fd7be2261dc60c53ac9abba098dd62676
Fred Zabulon <fredzabulon133@gmail.com>
2025-03-23 20:30:34 +0300

    added transitions and modifications on the password visibility

--- src/components/authImage.jsx
+++ src/components/authImage.jsx
@@ -6,10 +6,10 @@ function authImage() {
         <h1 className="font-logo text-white text-center lg:hidden ">finance</h1>
         <div className='hidden lg:block relative rounded-xl  w-full h-[100%] overflow-hidden'>
           <img className='absolute  w-full h-full object-cover' src={auth_Image} alt="" srcset="" />
-          <div className='absolute top-[2%] left-[5%] text-white text-xl'>
+          <div className='absolute top-[2%] left-[5%] text-white text-xl opacity-70'>
             <h1 className="font-logo">finance</h1>
           </div>
-          <div className='w-[90%] absolute bottom-[2%] left-[5%] text-white'>
+          <div className='w-[90%] absolute bottom-[2%] left-[5%] text-white opacity-70'>
             <p className='font-h2'>Keep track of your money<br />and save for your future</p>
             <p className='text-[12px] mt-2.5'>Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.</p>
           </div>
--- src/pages/auth/sign_up.jsx
+++ src/pages/auth/sign_up.jsx
@@ -1,35 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { AuthImage } from '../../components/index.js'
+import { Eye, EyeOff } from 'lucide-react'
 
 function sign_up() {
+  const [isVisible, setIsVisible] = useState(false)
+  const handleVisibility = () => {
+    setIsVisible(!isVisible)
+  }
   return (
 <div className='lg:flex  w-[100vw] h-[100vh]'>
       <AuthImage />
       <div className='w-[100%] h-[100%] lg:w-[65%] grid place-items-center'>
-        <div className='bg-white w-[50%] rounded-lg'>
-          <div className='p-[4%]'>
-            <h1 className='font-h2 capitalize text-xl'>Sign Up</h1>
+              <div className='bg-white w-[70%] md:w-[50%] rounded-lg'>
+                <div className='p-[4%]'>
+                  <h1 className='font-h2 capitalize text-xl text-gray-900'>Sign up</h1>
             <form action="" className='mt-[2%]'>
-              <div>
-                <p className='text-[12px]'>Email</p>
-                <input className='border w-full p-1 rounded-lg' type="email" name="email" id="email" />
-              </div>
-              <div className='mt-2.5'>
-                <p className='text-[12px]'>Password</p>
-                <input className="border w-full p-1 rounded-lg " type="password" name="password" id="password" />
-              </div>
-
-              <button type="submit" className='mt-5 text-white bg-black w-full p-2 rounded-lg'>Login</button>
-            </form>
-            <div className="mt-2.5 text-center">
-              <span className='text-[12px]'>Need to create an account ?</span> <Link to="/sign-up">Sign up</Link>
-            </div>
-          </div>
-            
-        </div>
-      </div>
+                    <div>
+                      <p className='text-[12px] text-gray-500'>Name</p>
+                      <input className='invalid:border-1 invalid:border-red-800  border border-[#98908B] w-full p-1 rounded-md  focus:border-transparent focus:outline-1 focus:outline-sky-600 focus:invalid:outline-red-800 transition-all duration-200 ease-in-out focus:transition-all focus:duration-200 focus:ease-in-out' type="text" name="name" id="name" />
+                    </div>              
+                    <div className='mt-2.5'>
+                      <p className='text-[12px] text-gray-500'>Email</p>
+                      <input className='invalid:border-1 invalid:border-red-800  border border-[#98908B] w-full p-1 rounded-md  focus:border-transparent focus:outline-1 focus:outline-sky-600 focus:invalid:outline-red-800 transition-all duration-200 ease-in-out focus:transition-all focus:duration-200 focus:ease-in-out' type="email" name="email" id="email" />
+                    </div>
+                    <div className='mt-2.5'>
+                      <p className='text-[12px] text-gray-500'>Password</p>
+                      <div className="relative">
+                        <input className={` border border-[#98908B] w-full p-1 rounded-md  focus:border-transparent focus:outline-1 focus:outline-sky-600 transition-all duration-200 ease-in-out focus:transition-all focus:duration-200 focus:ease-in-out`} type={isVisible ? "text":"password"} name="password" id="password" /> 
+                        {isVisible ? <EyeOff size={18} className='icon-position cursor-pointer' onClick={handleVisibility} /> : <Eye size={18} className='icon-position cursor-pointer' onClick={handleVisibility} />}
+                                               
+                      </div>
+                      <p className=' text-[12px] text-gray-500 text-right'>Password must be at least 8 characters</p>
+                    </div>
       
+                    <button type="submit" className='mt-5 text-white bg-gray-900 w-full p-2 rounded-md cursor-pointer'>Create Account</button>
+                  </form>
+                  <div className="mt-2.5 text-center">
+                    <span className='text-[12px] text-gray-500'>Already have an account ?</span> <Link to="/login" className='ml-2 capitalize text-sm underline text-gray-900 cursor-pointer'>Login</Link>
+                  </div>
+                </div>
+                  
+              </div>
+            </div>
     </div>
   )
 }
